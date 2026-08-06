6dd0d37a2a505ac8fb79bacdd9adeb23377bf78d
T-Sylivie <s.tumukunde@alustudent.com>
2026-08-06 02:07:52 +0200

    edit url

--- mobile/src/services/apiClient.js
+++ mobile/src/services/apiClient.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 /* import { API_BASE_URL } from '@env'; */
 import { getAccessToken } from './authService';
 
-const API_BASE_URL = 'http://10.0.2.2:8000';
+/* const API_BASE_URL = 'http://10.0.2.2:8000'; */
+
+const API_BASE_URL = 'https://uzima-backend.onrender.com';
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
-  timeout: 10000,
+  timeout: 60000,
 });
 
 apiClient.interceptors.request.use(async (config) => {
